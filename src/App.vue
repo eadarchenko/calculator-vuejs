9319a56cc8fea955a75710ae225f63f94908aeb7
Ekaterina Adarchenko <90846227+eadarchenko@users.noreply.github.com>
2022-08-19 13:34:12 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div id="app">
+     <calculator></calculator>
+  </div>
+
+
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+  Calculator
+  }
+}
+</script>
+
+<style>
+ body{
+  background: rgb(113, 113, 117);
+ }
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="p-3" style="max-width:400px; margin:50px auto; background: #234">
+  
+  <!--Calculator result-->
+  <div class="w-full rounded m-1 p-3  lead font-weight-bold  text-right  bg-vue-dark  text-white">
+    {{ calculatorValue || 0 }}
+  </div>
+
+  <!--Calculator buttons-->
+  <div class="row no-gutters">
+    <div class="col-3" v-for="n in calculatorElements" :key="n">
+     <div class="lead text-white text-center m-1 py-3 bg-vue-dark rounded hover-class"
+     :class="{'bg-vue-blue': ['C','*','/','-','+','%','='].includes(n)}"
+     @click="action(n)"
+     >
+      {{ n }}
+
+     </div>
+    </div>
+  </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Calculator',
+  props: {
+    msg: String
+  },
+
+  data(){
+    return{
+      calculatorValue:'',
+      calculatorElements:['C','*','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
+      operator : null,
+      previousCalculatorValue:'',
+    }
+  },
+  methods:{
+    
+    action(n){
+
+      /*Append value*/
+      if(!isNaN(n) || n === '.'){
+        this.calculatorValue += n + '';
+      }
+
+      /*Clear value*/
+    if(n=== 'C'){
+      this.calculatorValue = '';
+      }
+
+      if(n === '%'){
+        this.calculatorValue = this.calculatorValue /100 + '';
+      }
+
+      if(['/','*','-','+'].includes(n)){
+        this.operator = n;
+        this.previousCalculatorValue = this.calculatorValue;
+        this.calculatorValue = '';
+      }
+      if(n === '='){
+        this.calculatorValue = eval(
+          this.previousCalculatorValue +this.operator + this.calculatorValue);
+
+          this.previousCalculatorValue = '';
+          this.operator = null;
+      }
+    }
+
+    
+  }
+}
+</script>
+
+<style scoped>
+  .bg-vue-dark{
+    background: #31475e;
+  }
+  .hover-class::hover{
+    cursor:pointer;
+    background:rgb(222, 222, 241);
+  }
+  .bg-vue-blue{
+    background: rgb(124, 124, 236);
+  }
+
+</style>
